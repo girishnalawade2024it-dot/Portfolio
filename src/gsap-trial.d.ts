a1e4a3f4aff93ccdbc5f4a3ce2a9ed3d006fbe81
Nalawade Girish <nalawadegirish05@gmail.com>
2026-04-06 18:53:34 +0530

    fix: ignore ts checking on build

---
+++ src/gsap-trial.d.ts
@@ -0,0 +1,11 @@
+declare module 'gsap-trial/SplitText' {
+  const SplitText: any;
+  export default SplitText;
+  export { SplitText };
+}
+
+declare module 'gsap-trial/*' {
+  const content: any;
+  export default content;
+  export { content };
+}
